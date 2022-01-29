f0ee3045bbc62045a6c7fbebf3c8652a92d9fe20
danielfisher3 <38573896+danielfisher3@users.noreply.github.com>
2022-01-29 15:52:16 -0600

    Update

--- Global Game Jam 2022/Assets/_ProjectAssets/Scripts/Utils/BossManager.cs
+++ Global Game Jam 2022/Assets/_ProjectAssets/Scripts/Utils/BossManager.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class BossManager : MonoBehaviour
 {
     public bool juggernautActivated = false;
     public bool ogreActivated = false;
     public bool angelActivated = false;
     public bool mutantActivated = false;
-
+    [SerializeField] GameObject bossSlider;
     [SerializeField] GameObject juggerPrefab;
     [SerializeField] GameObject ogrePrefab;
     [SerializeField] GameObject angelPrefab;
@@ -27,6 +28,7 @@ public class BossManager : MonoBehaviour
     [SerializeField] JuggerNautAI jAI;
     [SerializeField] AngelAI aAI;
     [SerializeField] OgreAI oAI;
+    [SerializeField] MutantAI mAI;
 
     // Start is called before the first frame update
     void Start()
@@ -45,6 +47,9 @@ public class BossManager : MonoBehaviour
             if (jAI.dead == true)
             {
                 aWallAnimator.SetBool("JWallDown", true);
+
+                bossSlider.SetActive(false);
+                juggernautActivated = false;
             }
         }
         if (angelActivated)
@@ -52,6 +57,8 @@ public class BossManager : MonoBehaviour
             if(aAI.dead == true)
             {
                 aWallAnimator.SetBool("AWallDown", true);
+                bossSlider.SetActive(false);
+                angelActivated = false;
             }
         }
         if (ogreActivated)
@@ -59,6 +66,17 @@ public class BossManager : MonoBehaviour
             if(oAI.dead == true)
             {
                 aWallAnimator.SetBool("OWallDown", true);
+                bossSlider.SetActive(false);
+                ogreActivated = false;
+            }
+        }
+        if (mutantActivated)
+        {
+            if(mAI.dead == true)
+            {
+                aWallAnimator.SetBool("MWallDown", true);
+                bossSlider.SetActive(false);
+                mutantActivated = false;
             }
         }
 
@@ -79,6 +97,7 @@ public class BossManager : MonoBehaviour
             mActivator.SetActive(false);
             mArenaWall.SetActive(true);
             mutantPrefab.SetActive(true);
+            aWallAnimator = mArenaWall.GetComponent<Animator>();
             
 
         }
