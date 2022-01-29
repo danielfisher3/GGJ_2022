0bd2be460f0666e0b7581a25e0afa1b2d6680b09
danielfisher3 <38573896+danielfisher3@users.noreply.github.com>
2022-01-29 13:46:20 -0600

    Ogre_King Setup

--- Global Game Jam 2022/Assets/_ProjectAssets/Scripts/Enemy/BossAimEvents.cs
+++ Global Game Jam 2022/Assets/_ProjectAssets/Scripts/Enemy/BossAimEvents.cs
@@ -6,6 +6,7 @@ public class BossAimEvents : MonoBehaviour
 {
     [SerializeField]JuggerNautAI jAI = null;
     [SerializeField] AngelAI aAI = null;
+    [SerializeField] OgreAI oAI = null;
 
     [SerializeField] Collider attackCollider1 = null,attackCollider2 = null;
     
@@ -35,6 +36,16 @@ public class BossAimEvents : MonoBehaviour
             aAI.bossAnim.SetBool("Attack3", false);
 
             attackCollider1.enabled = false;
+            attackCollider2.enabled = false;
+        }
+        if(oAI != null)
+        {
+            oAI.bossAnim.SetBool("Attack1", false);
+            oAI.bossAnim.SetBool("Attack2", false);
+            oAI.bossAnim.SetBool("Attack3", false);
+
+            attackCollider1.enabled = false;
+            attackCollider2.enabled = false;
         }
     }
 }
